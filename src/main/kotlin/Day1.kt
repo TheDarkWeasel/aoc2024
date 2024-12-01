0ec40f9ad7b227763d999338ebd753a3c662c967
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2024-12-01 12:06:06 +0100

    Day1 Part2

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -5,15 +5,7 @@ import kotlin.math.abs
 class Day1 {
 
     fun solvePart1() {
-        val inputs = this::class.java.getResource("day1_1.txt")?.let { readFile(it.toURI()) }
-
-        val list1 = mutableListOf<Int>()
-        val list2 = mutableListOf<Int>()
-        inputs?.forEach {
-            val values = it.split("   ")
-            list1.add(values[0].toInt())
-            list2.add(values[1].toInt())
-        }
+        val (list1, list2) = readLists()
 
         list1.sortDescending()
         list2.sortDescending()
@@ -27,6 +19,31 @@ class Day1 {
         println("Day1 Part1: $result")
     }
 
+    fun solvePart2() {
+        val (list1, list2) = readLists()
+
+        val countMap = list2.groupingBy { it }.eachCount()
+
+        var result = 0
+        list1.forEach {
+            result += it * (countMap[it] ?: 0)
+        }
+
+        println("Day1 Part2: $result")
+    }
+
+    private fun readLists(): Pair<MutableList<Int>, MutableList<Int>> {
+        val inputs = this::class.java.getResource("day1_1.txt")?.let { readFile(it.toURI()) }
+
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        inputs?.forEach {
+            val values = it.split("   ")
+            list1.add(values[0].toInt())
+            list2.add(values[1].toInt())
+        }
+        return Pair(list1, list2)
+    }
 
     private fun readFile(fileName: URI): List<String> = File(fileName).readLines()
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
 fun main() {
     Day1().solvePart1()
+    Day1().solvePart2()
 }
